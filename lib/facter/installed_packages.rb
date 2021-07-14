f4ffb12e019b2eaa0f271fade69cc20c8319d363
Gavin Elder <gavin@improbable.io>
2021-07-14 20:30:35 +0100

    Mess about

--- lib/facter/installed_packages.rb
+++ lib/facter/installed_packages.rb
@@ -33,55 +33,25 @@ Facter.add("installed_packages") do
 
   setcode do
     if Facter.value(:os)["release"]["full"].to_i >= 10
-      require "json"
+      # Loop through all uninstall keys for 64bit applications.  
+      Win32::Registry::HKEY_LOCAL_MACHINE.open('Software\Microsoft\Windows\CurrentVersion\Uninstall') do |reg|
+        reg.each_key do |key|
+                
+        k = reg.open(key)
+        
+        displayname     = k["DisplayName"] rescue nil
+        version         = k["DisplayVersion"] rescue nil      
+        uninstallpath   = k["UninstallString"] rescue nil
+        systemcomponent = k["SystemComponent"] rescue nil
 
-      powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-
-      commands = [
-        'gp HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Convertto-json',
-        'gp HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Convertto-json',
-        'gp HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Convertto-json',
-      ]
-
-      if File.exist?(powershell)
-        installed_packages = {}
-
-        commands.each do |command|
-          raw = Facter::Util::Resolution.exec(%(#{powershell} -command "#{command}"))
-          next if raw.nil? || raw == ""
-
-          items = JSON.parse(raw)
-
-          if items.is_a?(Array)
-            items.each do |item|
-              next unless item.key?("DisplayName")
-
-              display_name = if item["DisplayName"].nil?
-                  ""
-                else
-                  item["DisplayName"].encode("UTF-8", "windows-1250")
-                end
-
-              installed_packages[display_name] = {
-                "version" => item["DisplayVersion"],
-                "installdate" => item["InstallDate"],
-              }
-            end
-          else
-            display_name = if items["DisplayName"].nil?
-                ""
-              else
-                items["DisplayName"].encode("UTF-8", "windows-1250")
+        if(displayname && uninstallpath)
+            unless(systemcomponent == 1)
+              unless(displayname.match(/[KB]{2}\d{7}/)) # excludes windows updates
+                software_list << {DisplayName: displayname, Version: version }
               end
-
-            installed_packages[display_name] = {
-              "version" => items["DisplayVersion"],
-              "installdate" => items["InstallDate"],
-            }
-          end
+            end
         end
-        installed_packages
+
+        end
       end
-    end
-  end
 end
