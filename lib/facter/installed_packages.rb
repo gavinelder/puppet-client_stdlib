f5b1c7b11d70059c2a7259afb58307db1a4fe229
Gavin Elder <gavin@improbable.io>
2021-10-04 19:53:37 +0100

    Update proper include

--- lib/facter/installed_packages.rb
+++ lib/facter/installed_packages.rb
@@ -32,7 +32,7 @@ Facter.add("installed_packages") do
   confine :kernel => "windows"
   setcode do
     require "win32/registry"
-    require 'puppet/type/registry_key'
+    require 'puppet/util/windows/registry'
 
     # Generate empty array to store hashes
     installed_packages = {}
