4299a6504fe875f3bacfb3d89ab22e73fa198b74
Gavin Elder <gavin@improbable.io>
2024-02-20 17:08:12 +0000

    Use Puppet4 + Syntax & Helpers

--- lib/facter/installed_packages.rb
+++ lib/facter/installed_packages.rb
@@ -26,97 +26,49 @@ Facter.add("installed_packages") do
   end
 end
 
-# yes, windows machines exist
-# Inspired from the following blog post & example https://tenfoursquid.com/getting-a-list-of-installed-software-in-windows-with-puppet/
 Facter.add("installed_packages") do
   confine :kernel => "windows"
   setcode do
-    require "puppet/util/windows"
+    require "puppet/util/windows/registry"
     include Puppet::Util::Windows::Registry
 
-    # Generate empty array to store hashes
     installed_packages = {}
 
-    # Check if reg path exist, return true / false
-    def key_exists?(path, scope)
-      begin
-        Win32::Registry::scope.open(path, ::Win32::Registry::KEY_READ)
-        return true
-      rescue
-        return false
-      end
-    end
-
-    # Loop through all uninstall keys for 64bit applications.
-    Win32::Registry::HKEY_LOCAL_MACHINE.open('Software\Microsoft\Windows\CurrentVersion\Uninstall') do |reg|
-      each_key(reg) do |key|
-        k = reg.open(key)
-        displayname = k["DisplayName"] rescue nil
-        version = k["DisplayVersion"] rescue nil
-        uninstallpath = k["UninstallString"] rescue nil
-        systemcomponent = k["SystemComponent"] rescue nil
-        installdate = k["InstallDate"] rescue nil
-
-        if (displayname && uninstallpath)
-          unless (systemcomponent == 1)
-            installed_packages[displayname] = {
-              "version" => version,
-              "installdate" => installdate,
-            }
-          end
-        end
-      end
-    end
-
-    # Loop through all uninstall keys for 32bit applications.
-    Win32::Registry::HKEY_LOCAL_MACHINE.open('Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall') do |reg|
-      each_key(reg) do |key|
-        k = reg.open(key)
-
-        displayname = k["DisplayName"] rescue nil
-        version = k["DisplayVersion"] rescue nil
-        uninstallpath = k["UninstallString"] rescue nil
-        systemcomponent = k["SystemComponent"] rescue nil
-        installdate = k["InstallDate"] rescue nil
-
-        if (displayname && uninstallpath)
-          unless (systemcomponent == 1)
-            installed_packages[displayname] = {
-              "version" => version,
-              "installdate" => installdate,
-            }
-          end
-        end
-      end
-    end
-
-    # Loop through all uninstall keys for user applications.
-    Win32::Registry::HKEY_USERS.open('\\') do |reg|
-      each_key(reg) do |sid|
-        unless (sid.include?("_Classes"))
-          path = "#{sid}\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall"
-          scope = "HKEY_USERS"
-          if key_exists?(path, scope)
-            Win32::Registry::scope.open(path) do |userreg|
-              userreg.each_key do |key|
-                k = userreg.open(key)
-                displayname = k["DisplayName"] rescue nil
-                version = k["DisplayVersion"] rescue nil
-                uninstallpath = k["UninstallString"] rescue nil
-                installdate = k["InstallDate"] rescue nil
-
-                if (displayname && uninstallpath)
-                  installed_packages[displayname] = {
-                    "version" => version,
-                    "installdate" => installdate,
-                  }
+    def each_installed_package
+      hives = ["HKEY_LOCAL_MACHINE", "HKEY_CURRENT_USER"]
+      paths = ['Software\Microsoft\Windows\CurrentVersion\Uninstall', 'Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall']
+      hives.each do |hive|
+        paths.each do |path|
+          begin
+            open(hive, path, KEY_READ | KEY_WOW64_64KEY) do |uninstall_key|
+              each_key(uninstall_key) do |subkey_name, _|
+                open(uninstall_key, subkey_name, KEY_READ) do |key|
+                  yield key
                 end
               end
             end
+          rescue Puppet::Util::Windows::Error => e
+            next if e.code == Puppet::Util::Windows::Error::ERROR_FILE_NOT_FOUND
           end
         end
       end
     end
+
+    each_installed_package do |key|
+      displayname = key["DisplayName"] rescue nil
+      version = key["DisplayVersion"] rescue nil
+      uninstallpath = key["UninstallString"] rescue nil
+      systemcomponent = key["SystemComponent"] rescue nil
+      installdate = key["InstallDate"] rescue nil
+
+      if displayname && uninstallpath && systemcomponent.to_i != 1
+        installed_packages[displayname] = {
+          "version" => version,
+          "installdate" => installdate,
+        }
+      end
+    end
+
     installed_packages
   end
 end
